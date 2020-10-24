e1ff74f5652afd88345b2ce0492a69bf112cbd83
MalcerOne <r.malcervelli@gmail.com>
2020-10-24 17:20:15 -0300

    add base_projeto e alterações

---
+++ meu_projeto/scripts/base_proj.py
@@ -0,0 +1,138 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from __future__ import print_function, division
+import numpy as np
+import numpy
+import tf, math, cv2, time, visao_module, rospy
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Image, CompressedImage
+from cv_bridge import CvBridge, CvBridgeError
+from numpy import linalg
+from tf import transformations
+from tf import TransformerROS
+import tf2_ros
+from geometry_msgs.msg import Twist, Vector3, Pose, Vector3Stamped
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Header
+import cv_bridge
+
+class Follower:
+  def __init__(self):
+    self.bridge = cv_bridge.CvBridge()
+    cv2.namedWindow("window", 1)
+    self.image_sub = rospy.Subscriber("/camera/image/compressed", Image, self.image_callback)
+
+  def image_callback(self, msg):
+    # BEGIN BRIDGE
+    image = self.bridge.compressed_imgmsg_to_cv2(msg)
+    # END BRIDGE
+    # BEGIN HSV
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    # END HSV
+    # BEGIN FILTER
+    lower_yellow = numpy.array([ 50,  50, 170])
+    upper_yellow = numpy.array([255, 255, 190])
+    mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+    # END FILTER
+    masked = cv2.bitwise_and(image, image, mask=mask)
+    cv2.imshow("window", mask ) 
+    cv2.waitKey(3)
+
+bridge = CvBridge()
+
+cv_image = None
+media = []
+centro = []
+atraso = 1.5E9 # 1 segundo e meio. Em nanossegundos
+
+
+area = 0.0 # Variavel com a area do maior contorno
+
+# Só usar se os relógios ROS da Raspberry e do Linux desktop estiverem sincronizados. 
+# Descarta imagens que chegam atrasadas demais
+check_delay = False 
+
+resultados = [] # Criacao de uma variavel global para guardar os resultados vistos
+
+x = 0
+y = 0
+z = 0 
+id = 0
+
+frame = "camera_link"
+
+tfl = 0
+
+tf_buffer = tf2_ros.Buffer()
+
+# A função a seguir é chamada sempre que chega um novo frame
+def roda_todo_frame(imagem):
+    print("frame")
+    global cv_image
+    global media
+    global centro
+    global resultados
+
+    now = rospy.get_rostime()
+    imgtime = imagem.header.stamp
+    lag = now-imgtime # calcula o lag
+    delay = lag.nsecs
+    # print("delay ", "{:.3f}".format(delay/1.0E9))
+    if delay > atraso and check_delay==True:
+        print("Descartando por causa do delay do frame:", delay)
+        return 
+    try:
+        follower = Follower()
+        antes = time.clock()
+        temp_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
+        # Note que os resultados já são guardados automaticamente na variável
+        # chamada resultados
+        centro, saida_net, resultados =  visao_module.processa(temp_image)        
+        for r in resultados:
+            # print(r) - print feito para documentar e entender
+            # o resultado            
+            pass
+
+        depois = time.clock()
+        # Desnecessário - Hough e MobileNet já abrem janelas
+        cv_image = saida_net.copy()
+        cv2.imshow("cv_image", cv_image)
+        cv2.waitKey(1)
+    except CvBridgeError as e:
+        print('ex', e)
+    
+if __name__=="__main__":
+    rospy.init_node("cor")
+    
+
+    topico_imagem = "/camera/image/compressed"
+
+    recebedor = rospy.Subscriber(topico_imagem, CompressedImage, roda_todo_frame, queue_size=4, buff_size = 2**24)
+
+
+    print("Usando ", topico_imagem)
+
+    velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
+
+    tfl = tf2_ros.TransformListener(tf_buffer) #conversao do sistema de coordenadas 
+    tolerancia = 25
+
+    # Exemplo de categoria de resultados
+    # [('chair', 86.965459585189819, (90, 141), (177, 265))]
+
+    try:
+        # Inicializando - por default gira no sentido anti-horário
+        vel = Twist(Vector3(0,0,0), Vector3(0,0,math.pi/10.0))
+        
+        while not rospy.is_shutdown():
+            for r in resultados:
+                print(r)
+            velocidade_saida.publish(vel)
+
+            rospy.sleep(0.1)
+
+    except rospy.ROSInterruptException:
+        print("Ocorreu uma exceção com o rospy")
+
+
