111fff4b4f9d3a8924a04d34cff98f6eff361137
Tkacz <gabrielmt2@al.insper.edu.br>
2020-11-06 10:02:26 -0200

    furia campea

---
+++ meu_projeto/scripts/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, division
+
+__author__ = ["Gabriel Mitelman Tkacz", "Rafael Selcali Malcervelli"]
+
+import rospy
+import numpy as np
+import numpy
+import tf
+import math
+import cv2
+import time
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Image, CompressedImage
+from cv_bridge import CvBridge, CvBridgeError
+from numpy import linalg
+from tf import transformations
+from tf import TransformerROS
+import tf2_ros
+from geometry_msgs.msg import Twist, Vector3, Pose, Vector3Stamped
+
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Header
