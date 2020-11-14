b59b4e2f7a587d63b598fbdcc2efb004d16dfbac
MalcerOne <r.malcervelli@gmail.com>
2020-11-14 18:13:17 -0300

    centro de massa

--- meu_projeto/scripts/auxiliar.py
+++ meu_projeto/scripts/auxiliar.py
@@ -10,59 +10,10 @@ print("Este script não deve ser executado diretamente")
 
 from ipywidgets import widgets, interact, interactive, FloatSlider, IntSlider
 import numpy as np
-import cv2
+import cv2, masks
+from geometry_msgs.msg import Twist, Vector3
 
-
-
-
-def make_widgets_mat(m, n):
-    """
-        Makes a m rows x n columns 
-        matriz of  integer Jupyter Widgets
-        all values initialized to zero
-    """
-    list_elements = []
-    for i in range(m):
-        row = []
-        for j in range(n):
-            row.append(widgets.IntText(value=0))
-        list_elements.append(row)
-
-    rows = []
-    for row in list_elements:
-        rows.append(widgets.HBox(row))
-        
-    widgets_mat = widgets.VBox(rows)
-        
-    return list_elements, widgets_mat
-
-def make_widgets_mat_from_data(data):
-    """
-        Creates a matriz of int Widgets given 2D-data
-    """
-    n = len(data)
-    m = len(data[0])
-    elements, mat = makeMat(n, m)
-    for i in range(n):
-        for j in range(m):
-            elements[i][j].value = data[i][j]
-    return elements, mat
-            
-def make_np_from_widgets_list(widgets_list):
-    """
-        Takes as input a list of lists of widgets and initializes a matrix
-    """
-    widgets = widgets_list
-    n = len(widgets)
-    m = len(widgets[0])
-    array = np.zeros((n,m), dtype=np.float32)
-    for i in range(n):
-        for j in range(m):
-            array[i][j] = widgets[i][j].value
-    return array
-
-
-def convert_to_tuple(html_color):
+def convertToTuple(html_color):
     colors = html_color.split("#")[1]
     r = int(colors[0:2],16)
     g = int(colors[2:4],16)
@@ -76,7 +27,7 @@ def to_1px(tpl):
     img[0,0,2] = tpl[2]
     return img
 
-def to_hsv(html_color):
+def toHsv(html_color):
     tupla = convert_to_tuple(html_color)
     hsv = cv2.cvtColor(to_1px(tupla), cv2.COLOR_RGB2HSV)
     return hsv[0][0]
@@ -88,4 +39,58 @@ def ranges(value):
     hsv2[0] = min(180, hsv[0]+ 30)
     hsv[1:] = 50
     hsv2[1:] = 255
-    return hsv, hsv2 
+    return hsv, hsv2
+
+def crosshair(img, point, size, color):
+    """ Desenha um crosshair centrado no point.
+        point deve ser uma tupla (x,y)
+        color é uma tupla R,G,B uint8
+    """
+    x,y = point
+    cv2.line(img,(x - size,y),(x + size,y),color,5)
+    cv2.line(img,(x,y - size),(x, y + size),color,5)
+
+def colorFilter(bgr, low, high):
+    hsv = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, low, high)
+    kernel = np.ones((5,5),np.uint8)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    return mask  
+
+def massCenterColor(img_bgr_limpa, str_cor, img_bgr_visivel):
+    hsv_low, hsv_high = masks.criar_valores_mascaras(str_cor)
+    color_mask = filter_color(img_bgr_limpa, hsv_low, hsv_high)
+
+    if str_cor == "amarelo":
+        color_mask = restringir_window_size(img_bgr_limpa, color_mask, img_bgr_visivel, [30,60])
+
+    posicao_centro_massa = center_of_mass(color_mask) 
+    desenha_centro = crosshair(img_bgr_visivel, posicao_centro_massa, 8, (255,0,255))
+    return posicao_centro_massa
+
+def restrictWindowSize(img_bgr_limpa, mascara, img_bgr_visivel, list_xo_y0):
+    x0 = list_xo_y0[0]
+    # y0 = list_xo_y0[1]
+    y0 = 90
+    x1 = img_bgr_limpa.shape[1] - 10
+    y1 = img_bgr_limpa.shape[0] - 20
+
+    clipped = mascara[y0:y1, x0:x1]
+    cv2.rectangle(img_bgr_visivel, (x0, y0), (x1, y1), (255,0,0),2,cv2.LINE_AA) #desenha retangulo da área selecionada
+    return clipped
+
+def direction(img_bgr_limpa, str_cor, img_bgr_visivel):
+    try:
+        cm = centro_massa_cor(img_bgr_limpa, str_cor, img_bgr_visivel)
+        x_centro  = cm[0]
+
+        if x_centro < centro - incerteza:
+            return "virar esquerda"
+        elif x_centro > centro + incerteza:
+            return "virar direita"
+        else:
+            return "seguir reto"
+
+    except:
+        return "perdeu pista"
+
